93af48dc045c6e898aca1ff31bca9945f836c180
g.shchetinkin <g.shchetinkin@connectflexi.com>
2020-07-31 23:23:29 +0300

    Lesson 2 -adds 2

--- homework_2.py
+++ homework_2.py
@@ -3,13 +3,18 @@ from pprint import pprint
 from homework_2_hh_module import get_hh_vacancies
 from homework_2_superjob_module import get_superjob_vacancies
 
-page_count = int(input(f'Введите количество страниц: '))
+def get_current_vacancies():
+    page_count = int(input(f'Введите количество страниц: '))
+    vacancy_list = []
+    vacancy_list = vacancy_list + get_hh_vacancies('/search/vacancy?clusters=true&enable_snippets=true&text=Java+script+developer&schedule=remote&from=cluster_schedule&showClusters=false', page_count)
+    vacancy_list = vacancy_list + get_superjob_vacancies('/vacancy/search/?keywords=java%20script%20developer&remote_work_binary=2&click_from=facet', page_count)
 
-vacancy_list = []
-vacancy_list = vacancy_list + get_hh_vacancies('/search/vacancy?clusters=true&enable_snippets=true&text=Java+script+developer&schedule=remote&from=cluster_schedule&showClusters=false', page_count)
-vacancy_list = vacancy_list + get_superjob_vacancies('/vacancy/search/?keywords=java%20script%20developer&remote_work_binary=2&click_from=facet', page_count)
-columns = ["source", "name", "link", "source", "salary_min", "salary_max", "salary_currency"]
+    return vacancy_list
 
-data_frame = pd.DataFrame({column : [vacancy[column] for vacancy in vacancy_list] for column in columns})
-print(data_frame.info())
-data_frame.to_csv("homework_2_vacancies.csv")
+if __name__ == '__main__':
+    vacancy_list = get_current_vacancies()
+    columns = ["source", "name", "link", "source", "salary_min", "salary_max", "salary_currency"]
+
+    data_frame = pd.DataFrame({column : [vacancy[column] for vacancy in vacancy_list] for column in columns})
+    print(data_frame.info())
+    data_frame.to_csv("homework_2_vacancies.csv")
